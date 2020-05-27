20734541a4257a803595b02a9adc00725ce33fee
Nikolay Kim <fafhrd91@gmail.com>
2020-05-27 14:26:52 +0600

    add example

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,34 @@
 //! Redis client for ntex framework.
+//!
+//! # Example
+//!
+//! ```rust
+//! use ntex_redis::{cmd, RedisConnector};
+//! # use rand::{thread_rng, Rng};
+//! # use rand::distributions::Alphanumeric;
+//! # fn gen_random_key() -> String {
+//! # let key: String = thread_rng().sample_iter(&Alphanumeric).take(12).collect();
+//! # key
+//! # }
+//!
+//! #[ntex::main]
+//! async fn main() -> Result<(), Box<dyn std::error::Error>> {
+//!     let redis = RedisConnector::new("127.0.0.1:6379").connect().await?;
+//!     let key = gen_random_key();
+//!
+//!     // create list with one value
+//!     redis.exec(cmd::LPush(&key, "value"));
+//!
+//!     // get value by index
+//!     let value = redis.exec(cmd::LIndex(&key, 0)).await?;
+//!     assert_eq!(value.unwrap(), "value");
+//!
+//!     // remove key
+//!     redis.exec(cmd::Del(&key)).await?;
+//!
+//!     Ok(())
+//! }
+//! ```
 mod client;
 pub mod cmd;
 pub mod codec;
